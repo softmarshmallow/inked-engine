48ce66e4666e53cd4f27af11a165ca9405bd9879
woojoo <woojoo@softmarshmallow.com>
2020-02-27 04:59:49 +0900

    minified logging & added s3 static serving

--- server/settings/base.py
+++ server/settings/base.py
@@ -99,7 +99,7 @@ DATABASES = {
             'version': 1,
             'loggers': {
                 'djongo': {
-                    'level': 'DEBUG',
+                    'level': 'ERROR',
                     'propogate': False,
                     'handlers': ['console']
                 }
@@ -107,7 +107,7 @@ DATABASES = {
             'handlers': {
                 'console': {
                     'class': 'logging.StreamHandler',
-                    'level': 'DEBUG'
+                    'level': 'ERROR'
                 }
             }
         },
@@ -173,6 +173,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = "AKIAUMSBTS5KYZKBCN5P"
+AWS_SECRET_ACCESS_KEY = "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"
+AWS_STORAGE_BUCKET_NAME = "inked-service-server-statics"
+STATIC_URL = f'https://inked-service-server-statics.s3.ap-northeast-2.amazonaws.com/'
