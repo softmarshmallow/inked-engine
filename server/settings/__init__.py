f3fab8dbfd7ce0abc20c46844b888e3455409f0c
woojoo <woojoo@softmarshmallow.com>
2020-02-26 22:39:21 +0900

    edited default settings option

--- server/settings/__init__.py
+++ server/settings/__init__.py
@@ -1,1 +1,9 @@
+import os
 from .base import *
+
+# setting is production by default
+stage = os.getenv('STAGE', 'production')
+if stage is 'production':
+    from .production import *
+elif stage is 'development':
+    from .development import *
