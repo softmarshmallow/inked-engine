d0157843f914b16aefa7a55b384998f9aafc5cb6
woojoo <woojoo@softmarshmallow.com>
2020-02-28 07:16:55 +0900

    😖 added batch post

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 from . import views
+from .models import RawNews
+from .serializers import NewsSerializer
 
 router = routers.DefaultRouter()
 router.register(r'news', views.NewsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path(r'news/batch', views.NewsListCreateView.as_view())
+    path(r'news/batch', views.NewsListCreateView.as_view(queryset=RawNews.objects.all(), serializer_class=NewsSerializer))
 ]
