8782b641e0bdc8867dcfab3417dc090ca070d35c
woojoo <woojoo@softmarshmallow.com>
2020-02-28 07:05:53 +0900

    😖 added batch post

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'news', views.NewsViewSet)
-router.register(r'news/batch', views.NewsListCreateView)
+router.register(r'news/batch', views.NewsListCreateView, basename='news list')
 
 urlpatterns = [
     path('', include(router.urls)),
