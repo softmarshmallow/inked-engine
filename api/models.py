a4bccc2e3810b281865f9c983362abac9b96e572
woojoo <woojoo@softmarshmallow.com>
2020-02-28 08:58:47 +0900

    😖 updated validator

--- api/models.py
+++ api/models.py
@@ -8,7 +8,7 @@ class RawNews(models.Model):
     article_id = models.CharField(max_length=20)
     time = models.DateTimeField()
     title = models.CharField(max_length=200)
-    article_url = models.URLField(max_length=200)
+    article_url = models.URLField(max_length=200, unique=True)
     origin_url = models.URLField(max_length=200, null=True, blank=True)
     body_html = models.TextField()
     provider = models.CharField(max_length=20)
