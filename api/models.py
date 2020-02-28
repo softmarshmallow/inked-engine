07f4e4417983dac5aac698857db2b477c6d90a07
woojoo <woojoo@softmarshmallow.com>
2020-02-28 09:06:49 +0900

    😖 updated model maxchar

--- api/models.py
+++ api/models.py
@@ -5,10 +5,10 @@ class RawNews(models.Model):
     class Meta:
         db_table = 'raw'
     _id = models.ObjectIdField()
-    article_id = models.CharField(max_length=20)
+    article_id = models.CharField(max_length=100)
     time = models.DateTimeField()
-    title = models.CharField(max_length=200)
-    article_url = models.URLField(max_length=200, unique=True)
-    origin_url = models.URLField(max_length=200, null=True, blank=True)
+    title = models.CharField(max_length=500)
+    article_url = models.URLField(max_length=500, unique=True)
+    origin_url = models.URLField(max_length=500, null=True, blank=True)
     body_html = models.TextField()
-    provider = models.CharField(max_length=20)
+    provider = models.CharField(max_length=50)
