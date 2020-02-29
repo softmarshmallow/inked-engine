a78c06536d9792f4c5852ac324b6a8a94b169010
woojoo <woojoo@softmarshmallow.com>
2020-02-29 21:55:38 +0900

    😖 sse test #2

--- server/settings/base.py
+++ server/settings/base.py
@@ -120,6 +120,10 @@ DATABASES = {
 }
 
 
+# region sse
+EVENTSTREAM_ALLOW_ORIGIN = "*"
+# endregion
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
