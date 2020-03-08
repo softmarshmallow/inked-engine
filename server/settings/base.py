df7676cf2fe5818900de6643315ec08866ac318e
woojoo <woojoo@softmarshmallow.com>
2020-03-08 22:59:46 +0900

    😖 pagination size to 50

--- server/settings/base.py
+++ server/settings/base.py
@@ -159,7 +159,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'django_utils.drf_pagination_style.DataOnlyPagination',
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 50,
 }
 
 
