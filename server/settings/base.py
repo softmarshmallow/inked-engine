7bff521ddaaf026dc0f64c4c7805d0399037c4d1
Gin <gin@genoplan.com>
2020-03-26 12:40:49 +0900

    # updated todo's for public deploy

--- server/settings/base.py
+++ server/settings/base.py
@@ -176,12 +176,3 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = "AKIAUMSBTS5KYZKBCN5P"
-AWS_SECRET_ACCESS_KEY = "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"
-AWS_STORAGE_BUCKET_NAME = "inked-service-server-statics"
-STATIC_URL = f'https://inked-service-server-statics.s3.ap-northeast-2.amazonaws.com/'
